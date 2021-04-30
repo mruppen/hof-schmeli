57b4bb67dd71d1941cfe0bc68a4bc4698c601c50
Michael Ruppen <michael.ruppen@isolutions.ch>
2021-04-30 05:40:16 +0200

    Page schule complete

--- src/components/teachersList/index.tsx
+++ src/components/teachersList/index.tsx
@@ -1,25 +1,62 @@
-import preact from "preact";
-import { Container, useTheme, makeStyles } from "@material-ui/core";
-import { useContainerStyles } from "../../hooks";
-import { ColorsType } from "../../utils";
+import { Container, makeStyles, useTheme } from "@material-ui/core";
 import clsx from "clsx";
+import preact from "preact";
+import peopleSample from "../../data/images/peopleSample.png";
+import { useContainerStyles } from "../../hooks";
+import { TeaserModel } from "../../models/models";
+import { ColorsType } from "../../utils";
+import TeaserRow from "../teaserRow";
 
 const useStyles = makeStyles({
-    container: {
-        paddingTop: 112
-    }
-})
+  container: {
+    paddingTop: 112,
+    paddingBottom: 80,
+  },
+});
+
+const teasers: TeaserModel[] = [
+  {
+    image: peopleSample,
+    title: "Simone Maurer",
+    text:
+      "Kindergärtnerin, Primarlehrerin, Kinderyoga-Lehrerin, Landwirtin im Nebenerwerb mit Schafen, Eseln und Pferden. Genossenschafts-Präsidentin",
+    to: "mailto:simone.maurer@sbs.ch",
+    toText: "simone.maurer@sbs.ch",
+  },
+  {
+    image: peopleSample,
+    title: "Simone Maurer",
+    text:
+      "Kindergärtnerin, Primarlehrerin, Kinderyoga-Lehrerin, Landwirtin im Nebenerwerb mit Schafen, Eseln und Pferden. Genossenschafts-Präsidentin",
+    to: "mailto:simone.maurer@sbs.ch",
+    toText: "simone.maurer@sbs.ch",
+  },
+  {
+    image: peopleSample,
+    title: "Simone Maurer",
+    text:
+      "Kindergärtnerin, Primarlehrerin, Kinderyoga-Lehrerin, Landwirtin im Nebenerwerb mit Schafen, Eseln und Pferden. Genossenschafts-Präsidentin",
+    to: "mailto:simone.maurer@sbs.ch",
+    toText: "simone.maurer@sbs.ch",
+  },
+];
 
 export default function TeachersList(): preact.VNode {
-    const theme = useTheme();
-    const colors: ColorsType ={
-        color: theme.palette.yellow.contrastText,
-        backgroundColor: theme.palette.yellow.main
-    }
-    const containerClasses = useContainerStyles(_ => colors);
-    const classes = useStyles();
+  const theme = useTheme();
+  const colors: ColorsType = {
+    color: theme.palette.yellow.contrastText,
+    backgroundColor: theme.palette.yellow.main,
+  };
+  const containerClasses = useContainerStyles((_) => colors);
+  const classes = useStyles();
 
-    return(<Container disableGutters maxWidth={false} className={clsx(containerClasses.main, classes.container)}>
-        
-    </Container>);
+  return (
+    <Container
+      disableGutters
+      maxWidth={false}
+      className={clsx(containerClasses.main, classes.container)}
+    >
+      <TeaserRow teasers={teasers} palette="yellow" />
+    </Container>
+  );
 }
