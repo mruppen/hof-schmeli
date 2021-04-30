c2caca60408323e120fe35f7a66f4b5bee16b0ec
Michael Ruppen <michael.ruppen@isolutions.ch>
2021-04-30 05:40:16 +0200

    Page schule complete

--- src/routes/schule/index.tsx
+++ src/routes/schule/index.tsx
@@ -6,17 +6,18 @@ import {
   useTheme,
 } from "@material-ui/core";
 import preact from "preact";
+import ContentTeaserBg from "../../components/contentTeaserBg";
+import ContentTeaserLeft from "../../components/contentTeaserLeft";
 import Footer from "../../components/footer";
 import HeaderSchule from "../../components/headers/schule";
+import ImageGallery from "../../components/imageGallery";
 import PlainTeaser from "../../components/plainTeaser";
+import TeachersList from "../../components/teachersList";
+import TopicTeaser from "../../components/topicTeaser";
+import genossenschafter from "../../data/images/genossenschafter.png";
 import hahn from "../../data/images/hahn.png";
 import schnecke from "../../data/images/schnecke.png";
-import genossenschafter from "../../data/images/genossenschafter.png";
-import ContentTeaserLeft from "../../components/contentTeaserLeft";
-import ImageGallery from "../../components/imageGallery";
-import ContentTeaserBg from "../../components/contentTeaserBg";
-import { PaletteOrColorsType, invert, getColors } from "../../utils";
-import TopicTeaser from "../../components/topicTeaser";
+import { getColors, invert } from "../../utils";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -27,12 +28,11 @@ const useStyles = makeStyles(() =>
       marginTop: 92,
     },
     margin100: {
-      marginTop: -200
+      marginTop: -200,
     },
     margin112: {
-      marginTop: 112
-    }
-
+      marginTop: 112,
+    },
   })
 );
 
@@ -44,25 +44,59 @@ export default function Schule(): preact.VNode {
   return (
     <Container disableGutters maxWidth={false}>
       <HeaderSchule />
-      <ContentTeaserLeft palette="blue" title="Die Bauernhof Schule" image={hahn} imageClassName={classes.margin92}>
+      <ContentTeaserLeft
+        palette="blue"
+        title="Die Bauernhof Schule"
+        image={hahn}
+        imageClassName={classes.margin92}
+      >
         <Typography variant="body1">
-          Der Bio-Bauernhof Schmeli befindet sich auf der Biela, 15 Minuten vom Bahnhof Brig entfernt. Es ist ein Milchwirtschaftsbetrieb mit hofeigener Sennerei. Neben den Kühen leben hier auch Schweine, Hühner, Katzen und ein Hund. Esel, Schafe und Pferde eines anderen Betriebes gehören ebenfalls zur Schulgemeinschaft. Das Schulgebäude besteht aus sieben unterschiedlich grossen Zimmern und einer grossen Küche. Den Kindern stehen ausserdem mehrere Aussenplätze,
-          ein grosses Tipi und ein Schulgarten zur Verfügung.
-          Ein Teil des Schulalltags darf sich direkt auf dem Hof mit den verschiedenen Tieren und im Garten abspielen. Auch wöchentliche Ausflüge in den Wald gehören dazu.
+          Der Bio-Bauernhof Schmeli befindet sich auf der Biela, 15 Minuten vom
+          Bahnhof Brig entfernt. Es ist ein Milchwirtschaftsbetrieb mit
+          hofeigener Sennerei. Neben den Kühen leben hier auch Schweine, Hühner,
+          Katzen und ein Hund. Esel, Schafe und Pferde eines anderen Betriebes
+          gehören ebenfalls zur Schulgemeinschaft. Das Schulgebäude besteht aus
+          sieben unterschiedlich grossen Zimmern und einer grossen Küche. Den
+          Kindern stehen ausserdem mehrere Aussenplätze, ein grosses Tipi und
+          ein Schulgarten zur Verfügung. Ein Teil des Schulalltags darf sich
+          direkt auf dem Hof mit den verschiedenen Tieren und im Garten
+          abspielen. Auch wöchentliche Ausflüge in den Wald gehören dazu.
         </Typography>
       </ContentTeaserLeft>
       <ImageGallery />
       <ContentTeaserBg />
-      <TopicTeaser to="/home" toText="Replace with correct text" image={genossenschafter} title="Das sind wir" subtitle="Unsere Genossenschafter">
+      <TopicTeaser
+        to="/home"
+        toText="Replace with correct text"
+        image={genossenschafter}
+        title="Das sind wir"
+        subtitle="Unsere Genossenschafter"
+      >
         <Typography variant="body1">
-          Von Links nach rechts:<br />Simone Maurer<br />Benedikt Maurer<br />
+          Von Links nach rechts:
+          <br />
+          Simone Maurer
+          <br />
+          Benedikt Maurer
+          <br />
         </Typography>
       </TopicTeaser>
-      <ContentTeaserLeft palette={teacherTeaserColors} title="Das Lehrer-Team" image={schnecke} containerClassName={classes.margin112} imageClassName={classes.margin100}>
+      <ContentTeaserLeft
+        palette={teacherTeaserColors}
+        title="Das Lehrer-Team"
+        image={schnecke}
+        containerClassName={classes.margin112}
+        imageClassName={classes.margin100}
+      >
         <Typography variant="body1">
-          Wir sind ein Team aus engagierten und motivierten Lehrpersonen, die einen grossen Rucksack, prall gefüllt mit vielen und vielfältigen Erfahrungen, mitbringen. Uns steht ein Bauernpaar zur Seite, welches seine Arbeiten auf dem Hof Hand-in-Hand mit dem Schulbetrieb plant und die Kinder aktiv miteinbezieht.
+          Wir sind ein Team aus engagierten und motivierten Lehrpersonen, die
+          einen grossen Rucksack, prall gefüllt mit vielen und vielfältigen
+          Erfahrungen, mitbringen. Uns steht ein Bauernpaar zur Seite, welches
+          seine Arbeiten auf dem Hof Hand-in-Hand mit dem Schulbetrieb plant und
+          die Kinder aktiv miteinbezieht.
         </Typography>
       </ContentTeaserLeft>
+      <TeachersList />
       <PlainTeaser to="/vision" toText="Unsere Vision">
         Wir leben Schule - Mit Menschlichkeit
       </PlainTeaser>
